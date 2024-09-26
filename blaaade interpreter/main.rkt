7b1110c010faea535915e76ed371a51ad3d427a3
aschneider15 <aschneider15@outlook.com>
2024-09-26 09:18:29 -0500

    Fixed upload for Assignment 8
    Forgot to save the main.rkt file, which led to the creation of a main.1 file... not what I intended.

--- blaaade interpreter/main.rkt
+++ blaaade interpreter/main.rkt
@@ -21,3 +21,12 @@
 ;(ask (a == 1) b x) -> (ask-exp (boolean-exp (var-exp a) (op ==) (num-exp 1))
 ; (true-exp (var-exp b))
 ; (false-exp (var-exp x))
+
+
+; Assignment 8 -  Implement a parser and complete numeric and math expressions so that the interpreter can execute the math expression result.
+
+; Expected output: (app-exp (func-exp (params x) (body-exp (math-exp (var-exp x) (op *) (num-exp 2)))) (var-exp a))
+(blaaade-parser '(call (function (x) (x * 2)) a))
+
+; Expected output: 2
+(execute '(call (function (x) (x  * 2)) a))
