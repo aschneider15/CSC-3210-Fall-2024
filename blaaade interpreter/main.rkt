44b76a32abf86195808744dc4139439092ecddc4
aschneider15 <aschneider15@outlook.com>
2024-10-15 22:29:21 -0500

    Assignment 12 complete. Added to new branch "dev"
    Going forward, my most recent homework assignments will be uploaded via the dev branch, and once it's been graded, I'll be able to merge it into main so that the next homework can be put into the dev branch.

--- blaaade interpreter/main.rkt
+++ blaaade interpreter/main.rkt
@@ -13,8 +13,7 @@
     )
   )
 
-;(wahl-exp (boolean-exp (var-exp a) (op <) (num-exp 10)
-;(body-exp (queue-exp (put-exp (var-exp a) (math-exp (var-exp a) (op +)
-;(num-exp 1)) (out-exp (var-exp a)))))
-(blaaade-parser '(rough (a < 10) 0 (a + 1) (out a)))
-;(execute '(wahl (a < 10) (queue (put a = (a + 1)) (out a))))
+; expected output: '(wahl-exp (boolean-exp (var-exp a) (op <) (num-exp 10)) (body-exp (queue-exp (put-exp (var-exp a) (math-exp (var-exp a) (op +) (num-exp 1))) (out-exp (var-exp a)))))
+(blaaade-parser '(wahl (a < 10) (queue (put a = (a + 1)) (out a))))
+; expected output: 1\n, 2\n, 3\n, 4\n, 5\n, 6\n, 7\n, 8\n, 9\n, 10\n, wahl-exp ends here
+(execute '(wahl (a < 10) (queue (put a = (a + 1)) (out a))))
